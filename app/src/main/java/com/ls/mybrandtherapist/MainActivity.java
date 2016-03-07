d292ad7188fd012b0234c75ccc9c5089a0e62f73
krishnakumar <softeng.krishna@gmail.com>
2016-03-07 11:41:02 +0530

    def

--- app/src/main/java/com/ls/mybrandtherapist/MainActivity.java
+++ app/src/main/java/com/ls/mybrandtherapist/MainActivity.java
@@ -3,12 +3,22 @@ package com.ls.mybrandtherapist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.ls.mybrandtherapist.widget.CircleMenuLayout;
 
+public class MainActivity extends AppCompatActivity {
+    private String[] mItemTexts = new String[] { "Blood Bank", "Result", "Settings",
+            "Clubs", "Share App", "About Us" };
+    private int[] mItemImgs = new int[] { R.drawable.ic_blood,
+            R.drawable.icon_result, R.drawable.icon_setting,
+            R.drawable.icon_reports, R.drawable.icon_share,
+            R.drawable.icon_aboutus };
+    CircleMenuLayout mCircleMenuLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mCircleMenuLayout = (CircleMenuLayout) findViewById(R.id.id_menulayout);
+        mCircleMenuLayout.setMenuItemIconsAndTexts(mItemImgs, mItemTexts);
     }
 }
